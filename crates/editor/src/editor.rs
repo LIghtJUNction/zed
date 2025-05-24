c4df222053d416d4af8298771c2748514b885076
Kirill Bulatov <kirill@zed.dev>
2025-05-24 17:45:49 +0300

    Add basic buttons

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -16005,7 +16005,7 @@ impl Editor {
         })
     }
 
-    fn restart_language_server(
+    pub fn restart_language_server(
         &mut self,
         _: &RestartLanguageServer,
         _: &mut Window,
@@ -16023,7 +16023,7 @@ impl Editor {
         }
     }
 
-    fn stop_language_server(
+    pub fn stop_language_server(
         &mut self,
         _: &StopLanguageServer,
         _: &mut Window,
